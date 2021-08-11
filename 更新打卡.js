44d2d88d8b7727f1c96265e2e4da475b03d4786c
robyonDevOpsServer <robortly@163.com>
2021-08-11 09:07:18 +0800

    整理更新打卡

--- 更新打卡.js
+++ 更新打卡.js
@@ -24,20 +24,20 @@ if (closeapp == 0) {
     var ret=device.restartApp(runAppName);
     if (ret == 0) {
         print("成功重启钉钉中...");
-        device.delay(15*1000);
+        device.delay(22*1000);
         print("预计处理完毕！");
         print("--------准备点击智能工作助理----------");
-        device.click(0.1536,0.4591); 
+        device.click(0.1058,0.3975); //Xiaomi-Redmi 4A	Input	click	0.1058, 0.3975
         print("--------点击完毕----------");
-        device.delay(13*1000);
+        device.delay(20*1000);
         print("--------准备点击去打卡----------");
         device.click(0.1120,0.8816); 
         print("--------点击完毕----------");
-        device.delay(18*1000); 
+        device.delay(20*1000); 
         print("--------准备点击更新打卡----------");
         device.click(0.8120,0.3519); 
         print("--------点击完毕----------");
-        device.delay(8*1000); 
+        device.delay(3*1000); 
         print("--------准备点击确定更新----------");
         device.click(0.7755,0.5660); 
         print("--------点击完毕----------");
